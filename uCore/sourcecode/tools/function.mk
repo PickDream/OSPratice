1a2a4cbf195bb23e169ee69466b6fa541b22d47f
PickDream <lumaoxin@aliyun.com>
2018-10-03 11:58:26 +0800

    小进展

--- uCore/sourcecode/tools/function.mk
+++ uCore/sourcecode/tools/function.mk
@@ -5,14 +5,15 @@ OBJPREFIX	:= __objs_
 # -------------------函数开始 --------------------
 
 # 列举指定目录下的所有指定类型的文件 (#directories, #types)
-# filter函数 先指明过滤规则，在指明过滤内容
+# [分析]filter函数 先指明过滤规则，在指明过滤内容
 # addperfix函数,如果指明了type参数的话,将.作为前缀加入到#type参数之前，构成%.type的类型，作为filter的过滤规则
+# 如果没有指明type参数的话，就直接返回通配符
 # $(SLASH)是在Makefile主文件定义的斜线,这里为目录名添加后缀，#directories/*
 # 经过函数处理的返回的是连带通配符(bash)的字串，不是文件名称，所以需要wildcard函数来进行转化
 listf = $(filter $(if $(2),$(addprefix %.,$(2)),%),\
 		  $(wildcard $(addsuffix $(SLASH)*,$(1))))
 
-# 得到所有的.o文件: (#files[, packet])
+# 传入文件列表得到所有的.o文件: (#files[, packet])
 #$(OBJDIR)是定义在Makefile主文件的变量，OBJDIR	:= obj
 # basename是取前缀函数,将其传入的后缀都干掉：$(basename src/foo.c src-1.0/bar.c hacks)返回值是“src/foo src-1.0/bar hacks”。
 # 传入的文件参数都会去掉原有的后缀名之后加上.o作为后缀，之后再加上前缀
@@ -20,16 +21,24 @@ listf = $(filter $(if $(2),$(addprefix %.,$(2)),%),\
 toobj = $(addprefix $(OBJDIR)$(SLASH)$(if $(2),$(2)$(SLASH)),\
 		$(addsuffix .o,$(basename $(1))))
 
-# get .d dependency files: (#files[, packet])
+# 将.o文件转化位.d 文件(#files[, packet])
+# patsubst 模式字符串替换函数,将%.o的文件转换为%.d的文件
+# %.d的文件列表来自上面写的toobj函数
 todep = $(patsubst %.o,%.d,$(call toobj,$(1),$(2)))
 
+#为传入的变量添加bin/前缀
+#$(BINDIR)这个变量来源于Makefile主文件 BINDIR	:= bin
 totarget = $(addprefix $(BINDIR)$(SLASH),$(1))
 
-# change $(name) to $(OBJPREFIX)$(name): (#names)
-# 该函数的目的是将文件名转化为$(OBJPREFIX)$(name)的形式,如果文件名不存在
+# 将 $(name) 转化为 $(OBJPREFIX)$(name)或者(#names)
+# 其中 OBJPREFIX:=__objs_
+# 该函数的目的是将文件名添加__objs_前缀的形式,如果传入为空就只返回__objs_
 packetname = $(if $(1),$(addprefix $(OBJPREFIX),$(1)),$(OBJPREFIX))
 
-# cc compile template, generate rule for dep, obj: (file, cc[, flags, dir])
+# cc 编译模版, 为dep, obj文件生成规则: (file, cc[, flags, dir])
+# 在define --endef定义块中
+# cc_template 在被调用之后，接受参数，组成一个makefile格式的文本，之后再去经过make
+# 的解释执行
 define cc_template
 $$(call todep,$(1),$(4)): $(1) | $$$$(dir $$$$@)
 	@$(2) -I$$(dir $(1)) $(3) -MM $$< -MT "$$(patsubst %.d,%.o,$$@) $$@"> $$@
